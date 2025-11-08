e27a689e5ac55a3321a47d3d296fb97d7d9c9f5d
Thuta180391 <132090808+Thuta180391@users.noreply.github.com>
2025-11-08 15:33:09 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+
+
+function setup() 
+{
+    createCanvas(512, 512);
+    fill(255,255,0);
+    noStroke();
+    
+}
+
+function draw()
+{
+    background(255);
+    
+    stroke(0,0,255);
+    fill(150,0,0);
+    rect(50,50,100, 100);
+
+    stroke(255,0,0);
+    fill(255,0,0);
+    ellipse(250,100,100,100);
+    
+    stroke(0,255,0);
+    fill(0,255,0,100);
+    ellipse(100,250,100,100);
+    rect(50,200,75,75);
+    
+    stroke(255,0,0)
+    fill(255,0,0,100);
+    ellipse(250,250,100,100);
+    rect(225,225,75,75);
+    
+}
+
+
