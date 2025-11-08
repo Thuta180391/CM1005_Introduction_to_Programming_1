942410944f6cd12a50292828faa6f3d74e551bf1
Thuta180391 <132090808+Thuta180391@users.noreply.github.com>
2025-11-08 13:17:01 +0100

    Shapes
    Drawing Shapes

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+
+function setup() 
+{
+    // Sets the screen to be 720 pixels wide and 400 pixels high
+    createCanvas(720, 400);
+
+}
+
+function draw()
+{
+    background(200);
+    noStroke();
+
+    fill(204, 101, 192, 127);
+    
+    triangle(18, 18, 18, 360, 81, 360);
+
+    rect(81, 81, 63, 63);
+
+    quad(200, 18, 250, 18, 350, 360, 2, 360);
+    
+    ellipse(252, 144, 72, 72);
+
+    triangle(288, 18, 351, 360, 288, 360); 
+
+    arc(479, 300, 280, 280, PI, TWO_PI);
+    
+    rect(100,100,30,30);
+}
+    
+// function setup()
+// {
+// createCanvas(500,500);
+// }
+// function draw()
+// {
+//     rect(100,100,30,40);
+//     rect(200,100,30,40);
+// }
+
+    
+
+
